0171627c500c3729e24fa7716088fd9a4042ec03
Greg Cunningham <greg@curioustech.net>
2016-11-09 23:13:47 -0500

    Create UdpTime.cpp

---
+++ libraries/UdpTime/UdpTime.cpp
@@ -0,0 +1,128 @@
+#include "Arduino.h"
+#include <TimeLib.h> // http://www.pjrc.com/teensy/td_libs_Time.html
+#include "UdpTime.h"
+
+UdpTime::UdpTime()
+{
+}
+
+void UdpTime::start()
+{
+  if(_bWaiting)
+    return;
+//  Serial.println("Udp start");
+  Udp.begin(2390);
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+  
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  // time.nist.gov
+  Udp.beginPacket("0.us.pool.ntp.org", 123); //NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
+  _bWaiting = true;
+  _bInit = true;
+}
+
+void UdpTime::DST() // 2016 starts 2AM Mar 13, ends Nov 6
+{
+  tmElements_t tm;
+  breakTime(now(), tm);
+  // save current time
+  uint8_t m = tm.Month;
+  int8_t d = tm.Day;
+  int8_t dow = tm.Wday;
+
+  tm.Month = 3; // set month = Mar
+  tm.Day = 14; // day of month = 14
+  breakTime(makeTime(tm), tm); // convert to get weekday
+
+  uint8_t day_of_mar = (7 - tm.Wday) + 8; // DST = 2nd Sunday
+
+  tm.Month = 11; // set month = Nov (0-11)
+  tm.Day = 7; // day of month = 7 (1-30)
+  breakTime(makeTime(tm), tm); // convert to get weekday
+
+  uint8_t day_of_nov = (7 - tm.Wday) + 1;
+
+  if ((m  >  3 && m < 11 ) ||
+      (m ==  3 && d > day_of_mar) ||
+      (m ==  3 && d == day_of_mar && hour() >= 2) ||  // DST starts 2nd Sunday of March;  2am
+      (m == 11 && d <  day_of_nov) ||
+      (m == 11 && d == day_of_nov && hour() < 2))   // DST ends 1st Sunday of November; 2am
+   _dst = 1;
+ else
+   _dst = 0;
+}
+
+uint8_t UdpTime::getDST()
+{
+  return _dst;
+}
+
+bool UdpTime::check(int8_t tz)
+{
+  if(_bInit == false) // never run?
+  {
+    start();
+    return false;
+  }
+  if(_bWaiting == false)
+    return false;
+  static int retry = 0;
+
+  if(!Udp.parsePacket())
+  {
+    if(++retry > 500)
+     {
+        _bWaiting = false;
+        start();
+        retry = 0;
+     }
+    return false;
+  }
+//  Serial.println("checkUdpTime good");
+
+  // We've received a packet, read the data from it
+  Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+  Udp.stop();
+  // the timestamp starts at byte 40 of the received packet and is four bytes,
+  // or two words, long. First, extract the two words:
+
+  unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+  unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+  unsigned long secsSince1900 = highWord << 16 | lowWord;
+  // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+  const unsigned long seventyYears = 2208988800UL;
+  long timeZoneOffset = 3600 * (tz + _dst);
+  unsigned long epoch = secsSince1900 - seventyYears + timeZoneOffset + 1; // bump 1 second
+
+  // Grab the fraction
+  highWord = word(packetBuffer[44], packetBuffer[45]);
+  lowWord = word(packetBuffer[46], packetBuffer[47]);
+  unsigned long d = (highWord << 16 | lowWord) / 4295000; // convert to ms
+  delay(d); // delay to next second (meh)
+  setTime(epoch);
+  DST(); // check the DST and reset clock
+  timeZoneOffset = 3600 * (tz + _dst);
+  epoch = secsSince1900 - seventyYears + timeZoneOffset + 1; // bump 1 second
+  setTime(epoch);
+
+//  Serial.print("Time ");
+//  Serial.println(timeFmt(true, true));
+  _bWaiting = false;
+  return true;
+}
